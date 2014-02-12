6ea71948d2703ab2bed6da7803fe6a97ccab6ead
isaactullis <irippletv@gmail.com>
2014-02-12 08:54:08 -0600

    I figured out how to sync Gits
    Yes!

---
+++ text.rb
@@ -0,0 +1,9 @@
+puts 'I am working on setting up gits'
+puts 'Am I awesome or am I AWESOME?! (A yes or no answer works fine)'
+response = gets.chomp
+
+if response == 'yes' || 'YES'
+	puts 'BAM!'
+	else
+	puts 'Um, please.'
+end
